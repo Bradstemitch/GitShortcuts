f046bc8438a694a63581a8e96f8ff6f0e0f265cd
bradstemitch <bradley.stephen.mitchell.95@gmail.com>
2020-12-04 17:13:32 +0000

    fixed typos

--- gitall.sh
+++ gitall.sh
@@ -7,7 +7,7 @@ if [[ "${branch}" == "master" ]]; then
 	echo "wrong branch"
 	exit 1
 fi
-if [[ ! -z {msg} ]]; then
+if [[ ! -z ${msg} ]]; then
 	git status
 	echo "pushing in 5"
 	sleep 5s
@@ -17,6 +17,6 @@ if [[ ! -z {msg} ]]; then
 	git commit -m "${msg}"
 	git push origin ${branch}
 else
-	echo "I need a message before I run"u
+	echo "I need a message before I run"
 	exit 1
 fi
